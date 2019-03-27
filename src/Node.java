676a1ff7a3f2e8c1c73c323a98c515d1dcb77396
Niranjan De Silva <desilvnira@ecs.vuw.ac.nz>
2019-03-27 20:50:49 +1300

    Fixed up my decsion tree builder so that I take into account the class types

--- src/Node.java
+++ src/Node.java
@@ -1,8 +1,17 @@
 
 public class Node {
 	
-	public Node(String className, double prob) {
+	String className;
+	double prob;
+	Node left;
+	Node right;
+	
+	public Node(String className, double prob, Node left, Node right) {
 		
+		this.className = className;
+		this.prob = prob;
+		this.left = left;
+		this.right = right;
 		
 		
 	}
--- src/Part2.java
+++ src/Part2.java
@@ -13,26 +13,29 @@ public class Part2 {
 		File trainingFile = new File(args[0]);
 		File testFile = new File(args[1]);
 		
+		List<String> attributes = new ArrayList<String>();
 		List<String> categoryNames = new ArrayList<String>();
 		
+		categoryNames.add("alive");
+		categoryNames.add("die");
 		
-		categoryNames.add("ALIVE");
-		categoryNames.add("AGE");
-		categoryNames.add("FEMALE");
-		categoryNames.add("STEROID");
-		categoryNames.add("ANTIVIRALS");
-		categoryNames.add("FATIGUE");
-		categoryNames.add("MALAISE");
-		categoryNames.add("ANOREXIA");
-		categoryNames.add("BIGLIVER");
-		categoryNames.add("FIRMLIVER");
-		categoryNames.add("SPLEENPALPABLE");
-		categoryNames.add("SPIDERS");
-		categoryNames.add("ASCITES");
-		categoryNames.add("VARICES");
-		categoryNames.add("BILIRUBIN");
-		categoryNames.add("SGOT");
-		categoryNames.add("HISTOLOGY");
+		
+		attributes.add("AGE");
+		attributes.add("FEMALE");
+		attributes.add("STEROID");
+		attributes.add("ANTIVIRALS");
+		attributes.add("FATIGUE");
+		attributes.add("MALAISE");
+		attributes.add("ANOREXIA");
+		attributes.add("BIGLIVER");
+		attributes.add("FIRMLIVER");
+		attributes.add("SPLEENPALPABLE");
+		attributes.add("SPIDERS");
+		attributes.add("ASCITES");
+		attributes.add("VARICES");
+		attributes.add("BILIRUBIN");
+		attributes.add("SGOT");
+		attributes.add("HISTOLOGY");
 		
 		List<Instance> instances = new ArrayList<Instance>();
 		try {
@@ -51,7 +54,7 @@ public class Part2 {
 			e.printStackTrace();
 		}
 		
-		buildTree(instances, categoryNames);
+		buildTree(instances, attributes);
 	}
 
 	public static Node buildTree(List<Instance> instances, List<String> attributes) {
@@ -71,27 +74,27 @@ public class Part2 {
 			}
 			double probability = Math.min(alive, dead)/Math.max(alive, dead);
 			if(alive > dead) {
-				return new Node("ALIVE", probability);
+				return new Node("ALIVE", probability, null, null);
 			}else {
-				return new Node("DEAD", probability);
+				return new Node("DEAD", probability, null, null);
 			}
 		}
 		
 		if(alive == 0 || dead == 0) {
 			if(alive == 0) {
-				return new Node("ALIVE", 1);
+				return new Node("ALIVE", 1, null, null);
 			}
 			if(dead == 0) {
-				return new Node("ALIVE", 1);
+				return new Node("ALIVE", 1, null, null);
 			}
 		}
 		
 		if(attributes.isEmpty()) {
 			double probability = Math.min(alive, dead)/Math.max(alive, dead);
 			if(alive > dead) {
-				return new Node("ALIVE", probability);
+				return new Node("ALIVE", probability, null, null);
 			}else {
-				return new Node("DEAD", probability);
+				return new Node("DEAD", probability, null, null);
 			}
 		}
 
@@ -101,8 +104,11 @@ public class Part2 {
 			List<Instance> bestInstaTrue = null;
 			List<Instance> bestInstaFalse = null;
 			
+			
 			Node left;
 			Node right;
+			
+			
 			
 			for (int pos = 0; pos < attributes.size(); pos++) {
 				List<Instance> t = new ArrayList<Instance>(); // instances for which the attribute is true
@@ -118,8 +124,9 @@ public class Part2 {
 					}
 				}
 				
-				double avgImpurity = (t.size() + f.size())/2; // calculates the avgImpurity of the current 2 sets
-				if(avgImpurity < bestAvgImpurity) {
+				double weightedAvgImpurity = weightedAvgImpurity(t, f, instances);
+				
+				if(weightedAvgImpurity < bestAvgImpurity) {
 					bestAtt = pos;
 					bestInstaTrue = t;
 					bestInstaFalse = f;
@@ -136,6 +143,42 @@ public class Part2 {
 		
 		return null;
 
+	}
+	
+	public static double weightedAvgImpurity(List<Instance> t, List<Instance> f, List<Instance> instances) {
+		
+		
+		int liveTrue = 0;
+		int dieTrue = 0;
+		int liveFalse = 0;
+		int dieFalse = 0;
+		
+		for(Instance i: t) {
+			if(i.getCategory() == 0) {
+				liveTrue++;
+			}else {
+				dieTrue++;
+			}
+		}
+		
+		for(Instance i: f) {
+			if(i.getCategory() == 0) {
+				liveFalse++;
+			}else {
+				dieFalse++;
+			}
+		}
+		
+		double trueImpurity = (liveTrue/t.size()) * (dieTrue/t.size());
+		double falseImpurity = (liveFalse/f.size()) * (dieFalse/f.size());
+		
+		double trueBranch = trueImpurity * (liveTrue + dieTrue/instances.size());
+		double falseBranch = falseImpurity * (liveFalse + dieFalse/instances.size());
+		
+		double weightedAvgImpurity = trueBranch + falseBranch;
+		
+		return weightedAvgImpurity;
+		
 	}
 
 }
